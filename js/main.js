b7d97b08bb9965791ad49b1e1d3182f5f85622c7
Maxim-coder-dotcom <59392284+Maxim-coder-dotcom@users.noreply.github.com>
2020-08-24 20:37:05 +0300

    burger menu

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ if(burgerTwo) {
 		menuBlock.style.right = '-1000px';
 	}
 }
+
+// mob menu
+
+let mobBurger = document.getElementById('mobBurger');
+if(mobBurger) {
+	let mobMenu = document.getElementById('mobMenu');
+
+	mobBurger.onclick = function() {
+		mobMenu.style.display = 'flex';
+	}
+}
+
+$(document).mouseup(function (e){ // событие клика по веб-документу
+        var div = $("#mobMenu"); // тут указываем ID элемента
+        if (!div.is(e.target) // если клик был не по нашему блоку
+            && div.has(e.target).length === 0) { // и не по его дочерним элементам
+            div.hide(); // скрываем его
+        }
+      });
+
